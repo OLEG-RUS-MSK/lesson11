611fa50bb8fea66e327bb4f799288aa7dd0a31e2
OLEG-RUS-MSK <logol-69@yandex.ru>
2024-09-22 14:18:46 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,6 @@
+date_num1 = 5
+date_num2 = 3
+date_sum = date_num1 + date_num2
+print(date_sum)
+date_result = date_sum + date_num1 * date_num2
+print(date_result)
---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = "Oleg"
+print("Name:", name)
+age = 54
+print('Age:', age)
+newAge = age + 1
+print("New Age:", newAge)
+is_student = bool(1==1)
+print("Is Student:", is_student)
