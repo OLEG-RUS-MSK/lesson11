2a82a071a0d7b4f85aae473e4859f64cf6e946ed
OLEG-RUS-MSK <logol-69@yandex.ru>
2024-10-03 23:55:01 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Наберите любой текст: ")
+print("Количество введёного текста:", len(my_string), "символов")
+print("Текст в верхнем регистре:", my_string.upper())
+print("Текст в нижнем регистре:", my_string.lower())
+print("Текст без пробелов:", my_string.replace(" ",""))
+print("Первый символ текста:", my_string[0])
+print("Последний символ текста:", my_string[-1])
